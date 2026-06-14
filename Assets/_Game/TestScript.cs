8e2a9b79a28b1d9b5e27e056119142ab90772de8
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2026-06-14 22:45:30 +0700

    Start project

---
+++ Assets/_Game/TestScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using R3;
+using VitalRouter;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ICommandPublisher _publisher;
+    [Button]
+    private void Test()
+    {
+        
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
